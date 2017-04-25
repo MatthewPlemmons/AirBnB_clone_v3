74cced9e348eb8d9b75e015fa847d8b11c39efa7
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-25 00:55:43 +0000

    api directory and files

---
+++ api/v1/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+if __name__ == "__main__":
+    HBNB_API_HOST = "0.0.0.0"
+    HBNB_API_PORT = "5000"
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT)
