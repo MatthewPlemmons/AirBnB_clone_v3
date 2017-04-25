7ce4fe0659dfe3f128c8c8af67150f7b3af1376c
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-25 14:15:44 +0000

    update app with 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(self):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify(error="Not found"), 404
 
 if __name__ == "__main__":
     HBNB_API_HOST = "0.0.0.0"
