85cc3bb1ea4eefda77d47515083326b00450b61a
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-25 04:14:30 +0000

    add endpoint to retrieve number of objects in models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """Number of objects for each model"""
+    stats = {}
+    for k, v in storage._DBStorage__models_available.items():
+       stats[v.__tablename__] = storage.count(k)
+    return jsonify(stats)
