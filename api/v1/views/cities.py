6537c983acc5441a9b59d143d9f7bb76175f3f06
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-28 23:24:34 +0000

    update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -42,31 +42,34 @@ def delete_city(city_id):
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
 def add_city(state_id):
     """Add a new City object to a State."""
+    r = request.get_json()
+    if r is None:
+        return jsonify(error='Not a JSON'), 400
+
     try:
-        r = request.get_json()
+        city = City(name=r['name']
+        city.state_id = state_id
+        city.save()
     except:
-        return jsonify("Not a JSON"), 400
-    if 'name' not in r.keys():
-        abort(400, {"Missing name"})
-    city = City(name=r['name'])
-    city.state_id = state_id
-    city.save()
+        return jsonify(error='Missing name'), 400
+
     return jsonify(city.to_json()), 201
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
 def update_city(city_id):
     """Update a City object."""
-    city = storage.get("City", city_id)
-    if city is None:
-        abort(404)
-
     try:
         r = request.get_json().items()
     except:
         return jsonify(error='Not a JSON'), 400
 
-    keys = ['id', 'state_id', 'created_at', 'updated_at']
-    {setattr(city, k, v) for k, v in r if k not in keys}
-    city.save()
+    try:
+        city = storage.get('City', city_id)
+        keys = ['id', 'state_id', 'created_at', 'updated_at']
+        {setattr(city, k, v) for k, v in r if k not in keys}
+        city.save()
+    except:
+        abort(404)
+
     return jsonify(city.to_json()), 200
