c02e3f9282b12f004c9512c1da32012d5c6cb52c
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-29 01:20:40 +0000

    update delete_city method

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -30,12 +30,11 @@ def get_city(city_id):
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
     """Delete a City object by it's ID."""
-    try:
-        city = storage.get("City", city_id)
-        storage.delete(city)
-        storage.save()
-    except:
+    city = storage.get('City', city_id)
+    if city is None:
         abort(404)
+    storage.delete(city)
+    storage.save()
     return jsonify({}), 200
 
 
