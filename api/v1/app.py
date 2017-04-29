ce4c5e519596d3e16dca73f887b74b4f2c9158ad
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-29 06:54:18 +0000

    update app.py to import flask_cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """API for the AirBnB clone application"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
